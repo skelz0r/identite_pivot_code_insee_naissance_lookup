a45d7b413b6b1a67fff1d6662f85e9f8c430a19c
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-13 09:33:50 +0200

    Enhance tests

--- spec/lib/code_insee_from_birth_place_and_date_spec.rb
+++ spec/lib/code_insee_from_birth_place_and_date_spec.rb
@@ -1,5 +1,6 @@
 require './lib/code_insee_from_birth_place_and_date'
 require 'csv'
+require 'json'
 
 RSpec.describe CodeINSEEForBirthDateAndPlace do
   before do
@@ -7,7 +8,9 @@ RSpec.describe CodeINSEEForBirthDateAndPlace do
   end
 
   CSV.foreach('./spec/examples.csv', headers: true) do |csv|
-    it "works for (#{csv['nom_commune']}, #{csv['annee_naissance']}, #{csv['departement_commune']}): #{csv['notes']}", vcr: { cassette_name: csv['code_insee'], record: :new_episodes } do
+    hexhash = Digest::SHA256.hexdigest(csv.to_h.slice('nom_commune', 'annee_naissance', 'departement_commune', 'code_insee').to_json)[0..6]
+
+    it "works for (#{csv['nom_commune']}, #{csv['annee_naissance']}, #{csv['departement_commune']}): #{csv['notes']}", vcr: { cassette_name: "#{hexhash}-#{csv['code_insee']}", record: :new_episodes } do
       status, payload = CodeINSEEForBirthDateAndPlace.new(csv['nom_commune'], csv['annee_naissance'], csv['departement_commune']).perform
 
       expect(status).to eq(200)
